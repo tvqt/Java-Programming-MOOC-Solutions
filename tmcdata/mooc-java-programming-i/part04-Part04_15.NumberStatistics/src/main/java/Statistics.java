9777f91383898ca95ab0ee94172b10adbba0df4f
Mark Hill <markh177@gmail.com>
2023-06-11 15:00:57 +1400

    more part/week 4

--- tmcdata/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,22 @@
 
 public class Statistics {
+    private int count;
+    private int sum;
+    public void addNumber(int number) {
+        this.sum += number;
+        this.count++;
+    }
+    public int getCount() {
+        return this.count;
+    }
+    public int sum() {
+        return this.sum;
+    }
+    public double average() {
+        if (this.count == 0) {
+            return 0;
+        }
+        return (double) this.sum / this.count;
+    }
+    
 }
--- tmcdata/mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_17.Items/src/main/java/Items.java
@@ -5,10 +5,19 @@ import java.util.Scanner;
 public class Items {
 
     public static void main(String[] args) {
-        // implement here your program that uses the class Item
-
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Name: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            items.add(new Item(name));
+        }
+        for (Item item : items) {
+            System.out.println(item);
+        }
 
     }
 }
